bb504c7b43055228ed2a390e87ecd3c9145172d6
nv4re <tosbodesz@gmail.com>
2023-08-30 12:24:44 +0700

    Add button show case

---
+++ .storybook/stories/Button/all.stories.ts
@@ -0,0 +1,112 @@
+import { spreadProps } from "@open-wc/lit-helpers";
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+import "../../../src/components/button/index.ts";
+import { Button } from "../../../src/components/button/index.ts";
+import { baseArgsTypes } from "../helper.ts";
+
+type ButtonWithLabel = Omit<
+  Button,
+  "variant" | "disabled" | "loading" | "size"
+> & {
+  label: string;
+};
+
+const variant: Button["variant"][] = ["solid", "outline", "ghost"];
+const state = [
+  {
+    disabled: false,
+    loading: false,
+  },
+  {
+    disabled: false,
+    loading: true,
+  },
+  {
+    disabled: true,
+    loading: false,
+  },
+  {
+    disabled: true,
+    loading: true,
+  },
+];
+const size: Button["size"][] = ["xl", "lg", "md", "sm", "xs"];
+
+const meta = {
+  title: "Example/Button",
+  tags: [],
+  render: ({ label, ...args }) => {
+    return html` <style>
+        main {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          gap: 2rem;
+          flex-flow: row wrap;
+        }
+
+        section {
+          display: flex;
+          flex-direction: column;
+          flex-flow: row nowrap;
+        }
+
+        div.container {
+          display: grid;
+          grid-template-columns: repeat(${state.length}, auto);
+          gap: 0.5em;
+        }
+      </style>
+      <main>
+        ${variant.map(
+          (v) => html`<section>
+            <div class="container">
+              ${size.map((sz) =>
+                state.map(
+                  (s) => html`<ssk-button
+                    ${spreadProps({ ...args, ...s, size: sz })}
+                    variant=${v}
+                    >${label}</ssk-button
+                  >`
+                )
+              )}
+            </div>
+          </section>`
+        )}
+      </main>`;
+  },
+  argTypes: {
+    label: {
+      description: "The content of the button",
+      control: "text",
+    },
+
+    hidden: {
+      control: {
+        type: "boolean",
+      },
+    },
+    onClick: {
+      action: "onClick",
+    },
+    ...baseArgsTypes,
+  },
+} satisfies Meta<ButtonWithLabel>;
+
+export default meta;
+
+type Story = StoryObj<ButtonWithLabel>;
+
+export const ShowCase: Story = {
+  args: {
+    themeColor: "primary",
+    label: "Button",
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/xKpB9x2tcu5FzWx25cQRJe/Design-System-SSK?type=design&node-id=1103%3A78477",
+    },
+  },
+};
--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -200,6 +200,7 @@ export class Button extends LitElement implements ThemeValue {
 
     if (this.variant === "ghost") {
       additionalCss += `
+
       button {
         background-color: transparent;
         border: none;
@@ -275,7 +276,7 @@ export class Button extends LitElement implements ThemeValue {
         @click=${!this.loading && this.onClick}
       >
         ${this.loading
-          ? svg`<svg width="1em" height="1em" stroke="var(--color)" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_V8m1{transform-origin:center;animation:spinner_zKoa 2s linear infinite}.spinner_V8m1 circle{stroke-linecap:round;animation:spinner_YpZS 1.5s ease-in-out infinite}@keyframes spinner_zKoa{100%{transform:rotate(360deg)}}@keyframes spinner_YpZS{0%{stroke-dasharray:0 150;stroke-dashoffset:0}47.5%{stroke-dasharray:42 150;stroke-dashoffset:-16}95%,100%{stroke-dasharray:42 150;stroke-dashoffset:-59}}</style><g class="spinner_V8m1"><circle cx="12" cy="12" r="9.5" fill="none" stroke-width="3"></circle></g></svg>`
+          ? svg`<svg width="1em" height="1em" stroke="currentColor" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_V8m1{transform-origin:center;animation:spinner_zKoa 2s linear infinite}.spinner_V8m1 circle{stroke-linecap:round;animation:spinner_YpZS 1.5s ease-in-out infinite}@keyframes spinner_zKoa{100%{transform:rotate(360deg)}}@keyframes spinner_YpZS{0%{stroke-dasharray:0 150;stroke-dashoffset:0}47.5%{stroke-dasharray:42 150;stroke-dashoffset:-16}95%,100%{stroke-dasharray:42 150;stroke-dashoffset:-59}}</style><g class="spinner_V8m1"><circle cx="12" cy="12" r="9.5" fill="none" stroke-width="3"></circle></g></svg>`
           : html`<slot name="prefix" />`}
         <slot />
         <slot name="postfix" />
