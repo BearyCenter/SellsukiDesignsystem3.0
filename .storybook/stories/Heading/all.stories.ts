c7dddb0a41ff582dd74131329fd18684e5c2dd40
nv4re <tosbodesz@gmail.com>
2023-09-01 12:32:38 +0700

    Adding heading

---
+++ .storybook/stories/Heading/all.stories.ts
@@ -0,0 +1,81 @@
+import { spreadProps } from "@open-wc/lit-helpers";
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+import "../../../src/components/heading/index.ts";
+import { Heading } from "../../../src/components/heading/index.ts";
+import { baseArgsTypes } from "../helper.ts";
+
+type HeadingArgs = {
+  content: string;
+} & Omit<Heading, "level">;
+
+// More on how to set up stories at: https://storybook.js.org/docs/web-components/writing-stories/introduction
+const meta = {
+  title: "Example/Heading",
+  tags: [],
+  render: ({ content, ...args }) => {
+    return html` <style>
+        div {
+        }
+      </style>
+      <div>
+        <ssk-heading ${spreadProps(args)} level="1">${content}</ssk-heading>
+        <ssk-heading ${spreadProps(args)} level="2">${content}</ssk-heading>
+        <ssk-heading ${spreadProps(args)} level="3">${content}</ssk-heading>
+        <ssk-heading ${spreadProps(args)} level="4">${content}</ssk-heading>
+        <ssk-heading ${spreadProps(args)} level="5">${content}</ssk-heading>
+      </div>`;
+  },
+  argTypes: {
+    content: {
+      control: {
+        type: "text",
+      },
+    },
+    italic: {
+      control: {
+        type: "boolean",
+      },
+    },
+    underline: {
+      control: {
+        type: "boolean",
+      },
+    },
+    strike: {
+      control: {
+        type: "boolean",
+      },
+    },
+    align: {
+      options: ["left", "center", "right", "justify"],
+      control: {
+        type: "select",
+      },
+    },
+    transform: {
+      options: ["uppercase", "lowercase", "capitalize"],
+      control: {
+        type: "inline-radio",
+      },
+    },
+    ...baseArgsTypes,
+  },
+} satisfies Meta<HeadingArgs>;
+
+export default meta;
+
+type Story = StoryObj<HeadingArgs>;
+
+// More on writing stories with args: https://storybook.js.org/docs/web-components/writing-stories/args
+export const ShowCase: Story = {
+  args: {
+    content: "Don't forget to be awesome!",
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/xKpB9x2tcu5FzWx25cQRJe/Design-System-SSK?type=design&node-id=801-59140",
+    },
+  },
+};
---
+++ src/components/heading/index.ts
@@ -0,0 +1,170 @@
+import { consume } from "@lit-labs/context";
+import { LitElement, TemplateResult, css, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import {
+  ColorName,
+  ColorRole,
+  FontFamilyGroup,
+  FontWeight,
+  Size,
+  Theme,
+} from "../../types/theme";
+import {
+  ThemeValue,
+  parseThemeValueComponentCss,
+} from "../../types/theme-value";
+import { themeContext } from "../theme-context";
+
+const levelToSize: Record<1 | 2 | 3 | 4 | 5, Size> = {
+  1: "4xl",
+  2: "3xl",
+  3: "2xl",
+  4: "xl",
+  5: "lg",
+};
+
+/**
+ * @slot - This element has a slot
+ * @csspart text
+ */
+@customElement("ssk-heading")
+export class Heading extends LitElement implements ThemeValue {
+  static registeredName = "ssk-heading";
+
+  @consume({ context: themeContext, subscribe: true })
+  @property({ attribute: false })
+  public theme?: Theme;
+
+  @property({ type: String })
+  themeColor: ColorRole | ColorName = "";
+  @property({ type: String })
+  color?: ColorRole | ColorName = "black.900";
+  @property({ type: String })
+  backgroundColor?: string | undefined;
+  @property({ type: String })
+  borderColor?: string | undefined;
+
+  @property({ type: String })
+  size: Size = "md";
+  @property({ type: String })
+  padding?: Size;
+  @property({ type: String })
+  fontSize?: string | undefined;
+  @property({ type: String })
+  lineHeight?: string | undefined;
+  @property({ type: String })
+  gap?: string | undefined;
+  @property({ type: String })
+  rounded?: string | undefined;
+  @property({ type: String })
+  margin?: string | undefined;
+
+  // font
+  @property({ type: String })
+  fontFamilyGroup: FontFamilyGroup = "sans";
+  @property({ type: String })
+  fontWeight: FontWeight = "normal";
+
+  @property({ type: String })
+  borderWidth?: string | undefined;
+  @property({ type: String })
+  boxShadow?: string | undefined;
+  @property({ type: String })
+  dropShadow?: string | undefined;
+  @property({ type: String })
+  width?: string | undefined;
+  @property({ type: String })
+  height?: string | undefined;
+  @property({ type: String })
+  minWidth?: string | undefined;
+  @property({ type: String })
+  minHeight?: string | undefined;
+  @property({ type: String })
+  maxWidth?: string | undefined;
+  @property({ type: String })
+  maxHeight?: string | undefined;
+
+  // text specific
+  @property({ type: Boolean })
+  hidden = false;
+
+  @property({ type: Boolean })
+  italic = false;
+
+  @property({ type: Boolean })
+  underline = false;
+
+  @property({ type: Boolean })
+  strike = false;
+
+  @property({ type: String })
+  align?: "left" | "center" | "right" | "justify" | undefined;
+
+  @property({ type: String })
+  transform?: "uppercase" | "lowercase" | "capitalize" | undefined;
+
+  @property({ type: Number })
+  level: 1 | 2 | 3 | 4 | 5 = 1;
+
+  static headings: Record<1 | 2 | 3 | 4 | 5, TemplateResult> = {
+    1: html`<h1><slot /></h1>`,
+    2: html`<h2><slot /></h2>`,
+    3: html`<h3><slot /></h3>`,
+    4: html`<h4><slot /></h4>`,
+    5: html`<h5><slot /></h5>`,
+  };
+
+  render() {
+    if (this.hidden) {
+      return html``;
+    }
+
+    let additionalCss = "";
+    let textDecorations = [];
+
+    if (this.italic) {
+      additionalCss += "font-style: italic;";
+    }
+
+    if (this.underline) {
+      textDecorations.push("underline");
+    }
+
+    if (this.strike) {
+      textDecorations.push("line-through");
+    }
+
+    if (this.transform) {
+      additionalCss += `text-transform: ${this.transform};`;
+    }
+
+    if (this.align) {
+      additionalCss += `text-align: ${this.align};`;
+    }
+
+    this.size = levelToSize[this.level];
+
+    return html`
+      <style>
+        h1, h2, h3, h4, h5 {
+          ${parseThemeValueComponentCss(this.theme, "heading", this)}
+          ${additionalCss}
+
+          ${textDecorations.length > 0
+          ? `text-decoration: ${textDecorations.join(" ")};`
+          : ""}
+        }
+      </style>
+
+      ${Heading.headings[this.level]}
+    `;
+  }
+
+  static styles = css``;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "ssk-heading": Heading;
+  }
+}
