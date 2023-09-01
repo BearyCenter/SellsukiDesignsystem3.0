2363c1bada218cefe3b2aec0d6f257f1928aa52d
nv4re <tosbodesz@gmail.com>
2023-09-01 13:23:50 +0700

    Adding build

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,30 @@
 // vite.config.js
+import path from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: "src/index.ts",
-      formats: ["es"],
+      entry: path.resolve(__dirname, "./src/main.ts"),
+      name: "Sellsuki Core Components",
+      // the proper extensions will be added
+      fileName: "sellsuki-core-components",
+      // formats: ["es"],
     },
     rollupOptions: {
       // If we want to publish standalone components we don't externalize lit,
       // if you are going to use lit in your own project, you can make it a dep instead.
       external: /^lit/, // <-- comment this line
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          lit: "lit",
+        },
+      },
     },
   },
+  plugins: [dts({ rollupTypes: true })],
 });
