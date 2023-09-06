3a880f205dfdfc084ef5457abc8baba50083c8c3
nv4re <tosbodesz@gmail.com>
2023-09-06 13:29:03 +0700

    Add storybook deps

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-designs",
+    "@storybook/addon-mdx-gfm"
   ],
   framework: {
     name: "@storybook/web-components-vite",
