45d5a178ba2afe20b1dec9d0ddae62114ff24ae1
nv4re <tosbodesz@gmail.com>
2024-01-02 23:27:28 +0700

    Give it some vibe

--- .storybook/stories/Icon/all.stories.ts
+++ .storybook/stories/Icon/all.stories.ts
@@ -3,45 +3,58 @@ import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
 import "../../../src/elements/icon";
 import { Icon } from "../../../src/elements/icon";
-import { baseArgsTypes, genericEvents } from "../helper";
+import { addPrefixToObject, baseArgsTypes, genericEvents } from "../helper";
 
-type IconArgs = Omit<Icon, "name">;
+type IconArgs = addPrefixToObject<Omit<Icon, "name">, "@">;
 
 const meta = {
   title: "Example/Icon",
   tags: [],
   render: ({ ...args }) => {
-    return html` <style>
+    return html`<style>
         main {
           width: 100%;
-          display: flex;
-          flex-flow: row wrap;
-          align-items: center;
+          display: grid;
+          grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
           justify-content: space-between;
-          gap: 1rem;
+          gap: 1em;
+        }
+
+        .icon-container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+          padding: 0.5em;
+          border-radius: 8px;
+          transition: transform 0.1s ease-in-out;
+        }
+
+        .icon-container:hover {
+          transform: scale(1.05);
+          box-shadow: 0 8px 16px rgba(0, 0, 0, 0.3);
+        }
+
+        .icon-name {
+          margin-top: 0.5em;
+          font-size: 0.8em;
+          text-align: center;
+          color: #333;
         }
       </style>
       <main>
         ${Object.keys(Icon.svgs).map(
-          (name) => html`<ssk-icon ${spread(args)} name=${name} />`,
+          (name) =>
+            html`<div class="icon-container">
+              <ssk-icon ${spread(args)} name=${name}></ssk-icon>
+              <div class="icon-name">${name}</div>
+            </div>`
         )}
       </main>`;
   },
   argTypes: {
     "@click": genericEvents["@click"],
-    "?spin": {
-      description: "When true gives the icon a spinner apparence",
-      table: {
-        category: "Props",
-        defaultValue: { summary: false },
-        type: {
-          summary: "boolean",
-        },
-      },
-      control: {
-        type: "boolean",
-      },
-    },
     ...baseArgsTypes,
   },
 } satisfies Meta<IconArgs>;
@@ -51,7 +64,10 @@ export default meta;
 type Story = StoryObj<IconArgs>;
 
 export const All: Story = {
-  args: {},
+  args: {
+    size: "3xl",
+    color: "#333",
+  },
   parameters: {
     design: {
       type: "figma",
