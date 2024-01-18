c8a6c4e71603453e3c8f917744cde20d1bfb0674
nv4re <tosbodesz@gmail.com>
2024-01-18 19:19:51 +0700

    side bar item size

--- src/components/sidebar/item.ts
+++ src/components/sidebar/item.ts
@@ -71,6 +71,7 @@ export class SidebarItems extends LitElement {
       padding: 12px 16px;
       cursor: pointer;
       border-radius: 8px;
+      font-size: 24px;
     }
 
     .sidebar-item-container.expanded {
