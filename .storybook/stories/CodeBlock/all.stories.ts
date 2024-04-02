698d43c67e9a32e57b73c6e25f251c92177bd93e
nv4re <tosbodesz@gmail.com>
2024-04-02 19:03:24 +0700

    Fix wrong indent

--- .storybook/stories/CodeBlock/all.stories.ts
+++ .storybook/stories/CodeBlock/all.stories.ts
@@ -31,19 +31,18 @@ export const ShowCase: Story = {
   args: {
     language: "json",
     code: `{
-      "activity_type": {
-        "id": "41cd2194-7554-462c-be2c-6144f68ecaf8",
-        "name": "oc2plus"
-      },
-      "created_at": "2022-11-24T04:16:26Z",
-      "error": "",
-      "status": {
-        "added": 50,
-        "duplicated": 10
-      },
-      "token": "3ks5t9sLpSobq4ieKBA68pu49I8C7G1UQJ95WZyrLvfIGKuHz5h54yCGm1yPXeH7dazdkjGUDRsl5CCr2qxfP1btEk94HGkElput2q5StrQif3hvQHpRlJWIpj"
-    }
-    `,
+  "activity_type": {
+    "id": "41cd2194-7554-462c-be2c-6144f68ecaf8",
+    "name": "oc2plus"
+  },
+  "created_at": "2022-11-24T04:16:26Z",
+  "error": "",
+  "status": {
+    "added": 50,
+    "duplicated": 10
+  },
+  "token": "3ks5t9sLpSobq4ieKBA68pu49I8C7G1UQJ95WZyrLvfIGKuHz5h54yCGm1yPXeH7dazdkjGUDRsl5CCr2qxfP1btEk94HGkElput2q5StrQif3hvQHpRlJWIpj"
+}`,
   },
   parameters: {
     design: {
