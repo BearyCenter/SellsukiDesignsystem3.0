d19008bac2ff21dfd7e995dcecc11cf349974390
nv4re <nv4rez@gmail.com>
2025-01-14 22:08:06 +0700

    production export

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,24 @@
-// vite.config.js
 import path from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "./src/main.ts"),
       name: "Sellsuki Components",
-      // the proper extensions will be added
       fileName: "sellsuki-components",
-      // formats: ["es"],
     },
     rollupOptions: {
-      // If we want to publish standalone components we don't externalize lit,
-      // if you are going to use lit in your own project, you can make it a dep instead.
-      external: /^lit/, // <-- comment this line
+      external: /^lit/,
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           lit: "lit",
         },
+      },
+      // Add resolve configuration
+      resolve: {
+        conditions: ["production"],
       },
     },
   },
