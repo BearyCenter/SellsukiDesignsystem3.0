4dc6c604e47f3a29338698e8b321e13ad60e2d57
arnon.pae <arnon.pae@sellsuki.com>
2025-01-30 17:04:27 +0700

    add textarea in error message

--- src/elements/textarea/index.ts
+++ src/elements/textarea/index.ts
@@ -190,7 +190,7 @@ export class Textarea extends LitElement {
           <label
             class="helper"
             data-testid=${this.testId
-              ? `${this.testId}.error-message`
+              ? `${this.testId}.textarea.error-message`
               : nothing}
           >
             ${this.helperText}
