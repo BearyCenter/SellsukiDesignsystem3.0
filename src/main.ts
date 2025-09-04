caa48f109fa401903995dbfeb26df3033433e211
Rattanakorn (Iced) Rengphian <rattanakorn.ren@sellsuki.com>
2025-09-04 16:45:13 +0700

    [➖] del not use

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,6 @@ export * from "./elements/widget-matric";
 
 export * from "./components/addon-phone-country";
 export * from "./components/calendar";
-export * from "./components/card";
 export * from "./components/card-expandable";
 export * from "./components/date-picker";
 export * from "./components/download-file";
