1d233507af0a6164c0a65f65556ae182d2b13fa2
kimzey <pan.nit@sellsuki.com>
2025-10-24 10:32:49 +0700

    update doc icon-wave > show label and subLabel

--- .storybook/stories/IconRipple/icon-wave.stories.ts
+++ .storybook/stories/IconRipple/icon-wave.stories.ts
@@ -3,18 +3,23 @@ import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
 import "../../../src/components/misc-icon";
 import { Icon } from "../../../src/elements/icon";
-import { baseArgsTypes, colors, customColors } from "../helper";
+import {
+  AutoLitProperty,
+  baseArgsTypes,
+  colors,
+  customColors,
+} from "../helper";
 import "../../../src/components/wave-icon";
 import type { WaveIcon } from "../../../src/components/wave-icon";
 import "../../../src/elements/text";
 
-type IconArgs = {} & WaveIcon;
+type IconArgs = AutoLitProperty<WaveIcon> & { label: string; subLabel: string };
 
 // More on how to set up stories at: https://storybook.js.org/docs/web-components/writing-stories/introduction
 const meta = {
   title: "Example/Misc Icon/Wave Icon",
   tags: ["autodocs"],
-  render: ({ ...args }) => {
+  render: ({ label, subLabel, ...args }) => {
     return html` <style>
         .message {
           text-align: center;
@@ -23,17 +28,22 @@ const meta = {
 
       <ssk-wave-icon ${spread(args)}>
         <div class="message">
-          <ssk-text size="xl" color="gray.800">
-            Slot for custom text in Wave Icon
-          </ssk-text>
-          <ssk-text color="gray.500">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-            commodo ligula eget dolor.
-          </ssk-text>
+          <ssk-text size="xl" color="gray.800"> ${label} </ssk-text>
+          <ssk-text color="gray.500"> ${subLabel} </ssk-text>
         </div>
       </ssk-wave-icon>`;
   },
   argTypes: {
+    label: {
+      description: "The main label text",
+      control: "text",
+      table: { category: "Props" },
+    },
+    subLabel: {
+      description: "The sub label text",
+      control: "text",
+      table: { category: "Props" },
+    },
     iconName: {
       description: "The icon's name",
       table: {
@@ -85,6 +95,8 @@ type Story = StoryObj<IconArgs>;
 // More on writing stories with args: https://storybook.js.org/docs/web-components/writing-stories/args
 export const Default: Story = {
   args: {
+    label: "Main Label",
+    subLabel: "Sub Label",
     iconName: "outline-academic-cap",
     themeColor: "primary",
     size: "xl",
